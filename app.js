8e5e2718ed1aebf4f60fb7ab25f756469783fc21
faisal hameed <152554660+faisah374@users.noreply.github.com>
2024-02-24 23:55:30 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+// question 2
+var myname = "faisal hameed";
+var massage = 'it is good personinalty';
+console.log(myname, massage);
+//question 1
+console.log("helloworld!");
---
+++ app.ts
@@ -0,0 +1,6 @@
+// question 2
+let myname ="faisal hameed";
+let massage = 'it is good personinalty';
+console.log(myname,massage);
+//question 1
+console.log("helloworld!");
